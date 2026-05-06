19ac3ff9c5acc10dd844577c9384864dc5533528
kyoenghwan <kyoenghwan0@gmail.com>
2026-05-06 17:39:13 +0900

    fix: 사이드 배너 전체 카드 비율을 1:2에서 1:1.5(2:3)로 수정

--- components/home/premium-job-card.tsx
+++ components/home/premium-job-card.tsx
@@ -147,7 +147,7 @@ export function PremiumJobCard({ company, title, location, pay, image, tags, isB
     }
 
     return (
-        <div className={`relative ${isBig ? 'h-full min-h-[292px]' : isSide ? 'aspect-[1/2]' : 'aspect-[3/2]'} w-full min-w-[140px] group p-[3px]`}>
+        <div className={`relative ${isBig ? 'h-full min-h-[292px]' : isSide ? 'aspect-[2/3]' : 'aspect-[3/2]'} w-full min-w-[140px] group p-[3px]`}>
             
             {/* --- [배로 아래 배경 레이어] --- */}
             {isImpact && (
