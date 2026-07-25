8e1db16869accce76f3201345c5ff592d4f6123d
kyoenghwan <kyoenghwan0@gmail.com>
2026-07-25 18:20:57 +0900

    fix(home): restore PC grid cols to original 4/4/5, only apply 3-col for tablet 600-799px

--- components/home/home-job-sections.tsx
+++ components/home/home-job-sections.tsx
@@ -242,7 +242,7 @@ export function HomeJobSections({ initialData }: HomeJobSectionsProps) {
                 </div>
                 
                 {demoJobs.length > 0 ? (
-                    <div className="grid grid-cols-2 min-[600px]:grid-cols-3 min-[800px]:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-6 gap-2 lg:gap-4 w-fit mx-auto">
+                    <div className="grid grid-cols-2 min-[600px]:grid-cols-3 min-[800px]:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-2 lg:gap-4 w-fit mx-auto">
                         {demoJobs.map((job, idx) => (
                             <div 
                                 key={job.id} 
@@ -295,7 +295,7 @@ export function HomeJobSections({ initialData }: HomeJobSectionsProps) {
                     </div>
                 </div>
                 {lineJobs.length > 0 ? (
-                    <div className="grid grid-cols-2 min-[600px]:grid-cols-3 min-[800px]:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-6 gap-2 lg:gap-4 w-fit mx-auto">
+                    <div className="grid grid-cols-2 min-[600px]:grid-cols-3 min-[800px]:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-2 lg:gap-4 w-fit mx-auto">
                         {lineJobs.map((job, idx) => (
                             <div 
                                 key={job.id} 
